4fe7d09ec11212276c3aed1fb141c9e0efbc6685
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2020-12-24 07:03:09 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -23,8 +23,14 @@ bot = Bot(token=bot_token)
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id,
-                             text="Hello beta tester, you can early access @NandiyaLive's projects' new features through this. These features are not yet ready for production, so there will be some errors. If you are having any, please send a message to @MDNChat. Use /now command to see what are the features curruntly running on this bot and know how to use them. \nJoin @MBNUpdates for more info.", parse_mode=telegram.ParseMode.HTML)
+    user = context.bot.get_chat_member(chat_id='-1001225141087', user_id=update.message.chat_id)
+    status = user["status"]
+    if(status == 'left'):
+        context.bot.send_message(chat_id=update.message.chat_id,text="To use to bot you need to be a member of @MBNUpdates in order to stay updated with the latest developments.")
+        return
+    else :
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                    text="Hello beta tester, you can early access @NandiyaLive's projects' new features through this. These features are not yet ready for production, so there will be some errors. If you are having any, please send a message to @MDNChat. Use /now command to see what are the features curruntly running on this bot and know how to use them. \nJoin @MBNUpdates for more info.", parse_mode=telegram.ParseMode.HTML)
 
 
 def now(update, context):
@@ -36,55 +42,61 @@ def echo(update, context):
 
 
 def feed(update, context):
-    fullmsg = update.message.text
-
-    if fullmsg == "/feed":
-        update.message.reply_text(
-            '/feed [instagram username]\nPlease read /help')
-    else:
-        msg = fullmsg.replace("/feed ", "")
-
-        if "@" in msg.lower():
-            query = msg.replace("@", "")
-        else:
-            query = msg
-
-    L = Instaloader(dirname_pattern=query, download_comments=False,
-                    download_video_thumbnails=False, save_metadata=False, download_geotags=True, compress_json=True, post_metadata_txt_pattern=None, storyitem_metadata_txt_pattern=None)
-    profile = Profile.from_username(L.context, query)
-
-    media = profile.mediacount
-    update.message.reply_text("Cooking your request 👨‍🍳\nProfile : " + query + "\nMedia Count : " + str(media) +
-                              "\nThis may take longer, take a nap I can handle this without you.")
-
-    posts = profile.get_posts()
-    try:
-        L.posts_download_loop(posts, query)
-    except Exception as e:
-        context.bot.send_message(chat_id=update.message.chat_id, text="<b>ERROR\n"+str(
-            e), parse_mode=telegram.ParseMode.HTML)
+    user = context.bot.get_chat_member(chat_id='-1001225141087', user_id=update.message.chat_id)
+    status = user["status"]
+    if(status == 'left'):
+        context.bot.send_message(chat_id=update.message.chat_id,text="To use to bot you need to be a member of @MBNUpdates in order to stay updated with the latest developments.")
         return
+    else :
+        fullmsg = update.message.text
 
-    update.message.reply_text("Download Completed.\n🗄 Archiving files...")
+        if fullmsg == "/feed":
+            update.message.reply_text(
+                '/feed [instagram username]\nPlease read /help')
+        else:
+            msg = fullmsg.replace("/feed ", "")
 
-    zf = zipfile.ZipFile(f"{query}.zip", "w")
-    for dirname, subdirs, files in os.walk(query):
-        zf.write(query)
-        for filename in files:
-            zf.write(os.path.join(dirname, filename))
-    zf.close()
+            if "@" in msg.lower():
+                query = msg.replace("@", "")
+            else:
+                query = msg
 
-    update.message.reply_text("Uploading to Telegram...")
+        L = Instaloader(dirname_pattern=query, download_comments=False,
+                        download_video_thumbnails=False, save_metadata=False, download_geotags=True, compress_json=True, post_metadata_txt_pattern=None, storyitem_metadata_txt_pattern=None)
+        profile = Profile.from_username(L.context, query)
 
-    for zip_file in glob.glob("*.zip"):
-        context.bot.send_document(chat_id=update.message.chat_id,
-                                  document=open(zip_file, 'rb'))
+        media = profile.mediacount
+        update.message.reply_text("Cooking your request 👨‍🍳\nProfile : " + query + "\nMedia Count : " + str(media) +
+                                "\nThis may take longer, take a nap I can handle this without you.")
 
-    try:
-        shutil.rmtree(query)
-        os.remove(f"{query}.zip")
-    except Exception:
-        pass
+        posts = profile.get_posts()
+        try:
+            L.posts_download_loop(posts, query)
+        except Exception as e:
+            context.bot.send_message(chat_id=update.message.chat_id, text="<b>ERROR\n"+str(
+                e), parse_mode=telegram.ParseMode.HTML)
+            return
+
+        update.message.reply_text("Download Completed.\n🗄 Archiving files...")
+
+        zf = zipfile.ZipFile(f"{query}.zip", "w")
+        for dirname, subdirs, files in os.walk(query):
+            zf.write(query)
+            for filename in files:
+                zf.write(os.path.join(dirname, filename))
+        zf.close()
+
+        update.message.reply_text("Uploading to Telegram...")
+
+        for zip_file in glob.glob("*.zip"):
+            context.bot.send_document(chat_id=update.message.chat_id,
+                                    document=open(zip_file, 'rb'))
+
+        try:
+            shutil.rmtree(query)
+            os.remove(f"{query}.zip")
+        except Exception:
+            pass
 
 
 def main():
